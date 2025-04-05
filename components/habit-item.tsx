30a114fc699458da0d12a6c4e33c2d211b352d87
Rohan <rsrohan787@gmail.com>
2025-04-05 10:07:59 +0530

    Updated checkbox

--- components/habit-item.tsx
+++ components/habit-item.tsx
@@ -25,6 +25,17 @@ export default function HabitItem({ habit, onToggle, onDelete, onEdit }: HabitIt
         habit.completed ? "bg-green-100" : "bg-white"
       }`}
     >
+      {/* LEFT: Habit name */}
+      <label
+        htmlFor={`habit-${habit.id}`}
+        className={`text-lg font-medium transition-colors duration-300 ${
+          habit.completed ? "text-green-600" : "text-[#333333]"
+        }`}
+      >
+        {habit.name}
+      </label>
+
+      {/* RIGHT: Checkbox + Dropdown */}
       <div className="flex items-center gap-3">
         <Checkbox
           id={`habit-${habit.id}`}
@@ -32,32 +43,25 @@ export default function HabitItem({ habit, onToggle, onDelete, onEdit }: HabitIt
           onCheckedChange={() => onToggle(habit.id)}
           className="border-[#7C5CFC] data-[state=checked]:bg-[#7C5CFC] data-[state=checked]:text-white"
         />
-        <label
-          htmlFor={`habit-${habit.id}`}
-          className={`text-lg font-medium transition-colors duration-300 ${
-            habit.completed ? "text-green-600" : "text-[#333333]"
-          }`}
-        >
-          {habit.name}
-        </label>
-      </div>
 
-      {/* Dropdown Menu */}
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <button className="text-gray-400 hover:text-black">
-            <MoreVertical size={18} />
-          </button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => onEdit?.(habit.id)} className="cursor-pointer">
-            <Pencil className="mr-2 h-4 w-4" /> Edit
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => onDelete(habit.id)} className="cursor-pointer text-red-600">
-            <Trash2 className="mr-2 h-4 w-4" /> Delete
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+        {/* Dropdown Menu */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="text-gray-400 hover:text-black">
+              <MoreVertical size={18} />
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => onEdit?.(habit.id)} className="cursor-pointer">
+              <Pencil className="mr-2 h-4 w-4" /> Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => onDelete(habit.id)} className="cursor-pointer text-red-600">
+              <Trash2 className="mr-2 h-4 w-4" /> Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   )
 }
+
